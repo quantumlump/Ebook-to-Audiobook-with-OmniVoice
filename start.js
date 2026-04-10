496432c17687ff6283fe4c5712dafafb5502fc40
quantumlump <48975716+quantumlump@users.noreply.github.com>
2026-04-10 07:05:03 -0700

    Update start.js

--- start.js
+++ start.js
@@ -11,7 +11,7 @@ module.exports = {
           OMNIVOICE_PORT: "{{port}}",
           HF_HUB_ENABLE_HF_TRANSFER: "1",
         },
-        message: ["python app/app.py"],
+        message: ["python -u app/app.py"],
         on: [
           {
             event: "/(http:\\/\\/[0-9.:]+)/",
