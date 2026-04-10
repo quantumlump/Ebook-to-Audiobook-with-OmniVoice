f28f13e1c5f1f58b585f99db0711620a21d2066a
quantumlump <48975716+quantumlump@users.noreply.github.com>
2026-04-10 08:43:27 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -65,7 +65,7 @@ import logging
 import sys
 
 logging.basicConfig(
-    level=logging.info,
+    level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.StreamHandler(sys.stdout)]
 )
