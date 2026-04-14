dc31aea32e0a7fd12ba4558333953ddc34cf84eb
quantumlump <48975716+quantumlump@users.noreply.github.com>
2026-04-13 17:22:07 -0700

    Update start.js

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 module.exports = {
   daemon: true,
-  run: [
+  run:[
     {
       method: "shell.run",
       params: {
@@ -10,9 +10,16 @@ module.exports = {
           PYTHONUNBUFFERED: "1",
           OMNIVOICE_PORT: "{{port}}",
           HF_HUB_ENABLE_HF_TRANSFER: "1",
+          // NEW VARIABLES: Force strict offline mode
+          HF_HUB_OFFLINE: "1",
+          TRANSFORMERS_OFFLINE: "1",
+          // Point to the local folder we downloaded the weights to in install.js
+          HF_HOME: "hf_cache",
+          // Stop Gradio from trying to ping its analytics servers
+          GRADIO_ANALYTICS_ENABLED: "False"
         },
         message: ["python -u app/app.py"],
-        on: [
+        on:[
           {
             event: "/(http:\\/\\/[0-9.:]+)/",
             done: true,
